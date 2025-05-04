5a58d182686b69a055428da37c15d2c621b04201
Nicole <nholt3@atu.edu>
2025-05-04 17:27:37 -0500

    Completed skills page

--- frontend/src/views/skills.js
+++ frontend/src/views/skills.js
@@ -1,18 +1,9 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import SkillsView from './skillsView';
 import Header from '../components/header';
 import Footer from '../components/footer';
 
 function Skills(){
-  useEffect(() => {
-      fetch('http://localhost:5000/skills')
-      .then(res => res.text())
-      .then(data => {
-        console.log('GET /skills response:', data);
-      })
-      .catch(err => console.error('GET /skills failed:', err));
-    }, []);
-
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
--- frontend/src/views/skillsView.js
+++ frontend/src/views/skillsView.js
@@ -1,9 +1,24 @@
+import React, {useEffect, useState} from 'react';
+
 function SkillsView() {
+    const [skills, setSkills] = useState([]);
+    
+    useEffect(() => {
+        fetch('http://localhost:5000/skills')
+        .then(res => res.json())
+        .then(data => setSkills(data))
+        .catch(err => console.error('Failed to fetch skills:', err));
+    }, []);
+
     return (
-      <main className="text-darkestcolor dark:text-lightestcolor">
-        <div className="bg-lightcolor dark:bg-darkcolor mx-auto my-10 rounded-xl border-6 border-accentcolor p-8 w-[90%] flex flex-col items-center gap-6">
-            <p>Testing</p>
-        </div>
+        <main className="text-darkestcolor dark:text-lightestcolor">
+        {skills.map((skills, index) => (
+            <div className="bg-lightcolor dark:bg-darkcolor mx-auto my-10 rounded-xl border-6 border-accentcolor p-8 w-[90%] flex flex-col items-center gap-6">
+                <h2 className='font-bold text-4xl text-center'>Skill: {skills.skill}</h2>
+                <p className='text-xl'>Proficiency: {skills.proficiency}</p>
+                {skills.icon && <img src={skills.icon} alt={skills.skill} className="bg-lightestcolor rounded-md" />}
+            </div>
+        ))}
       </main>
     );
 }
