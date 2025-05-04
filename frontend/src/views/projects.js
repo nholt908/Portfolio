d5b1bb7cf199730ef8bf2b7387fdd67ee7126207
Nicole <nholt3@atu.edu>
2025-05-04 17:13:26 -0500

    Completed projects page

--- frontend/src/views/projects.js
+++ frontend/src/views/projects.js
@@ -1,18 +1,9 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import ProjectsView from './projectsView';
 import Header from '../components/header';
 import Footer from '../components/footer';
 
 function Projects(){
-  useEffect(() => {
-      fetch('http://localhost:5000/projects')
-      .then(res => res.text())
-      .then(data => {
-        console.log('GET /projects response:', data);
-      })
-      .catch(err => console.error('GET /projects failed:', err));
-    }, []);
-
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
--- frontend/src/views/projectsView.js
+++ frontend/src/views/projectsView.js
@@ -1,9 +1,26 @@
+import React, {useEffect, useState} from 'react';
+
 function ProjectsView() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/projects')
+        .then(res => res.json())
+        .then(data => setProjects(data))
+        .catch(err => console.error('Failed to fetch projects:', err));
+    }, []);
+
     return (
       <main className="text-darkestcolor dark:text-lightestcolor">
-        <div className="bg-lightcolor dark:bg-darkcolor mx-auto my-10 rounded-xl border-6 border-accentcolor p-8 w-[90%] flex flex-col items-center gap-6">
-            <p>Testing</p>
-        </div>
+        {projects.map((project, index) => (
+            <div className="bg-lightcolor dark:bg-darkcolor mx-auto my-10 rounded-xl border-6 border-accentcolor p-8 w-[90%] flex flex-col items-center gap-6">
+                <h2 className='font-bold text-4xl text-center'>Project: {project.title}</h2>
+                <p className='text-xl'>{project.description}</p>
+                <p>Skills Used: {project.technologies}</p>
+                {project.image && <img src={project.image} alt={project.title} className="rounded-3xl max-w-[90%] max-h-[80%]" />}
+                <a href={project.GitHubLink} target="_blank" rel='noopener noreferrer' className='underline text-darkcolor dark:text-lightcolor'>GitHub Link</a>
+            </div>
+        ))}
       </main>
     );
 }
