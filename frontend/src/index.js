2e14989f457ce41eaec399593c6bea61a50f39a7
Nicole <nholt3@atu.edu>
2025-05-03 19:43:09 -0500

    created home page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './output.css';
-import App from './App';
+import IndexView from './IndexView';
 import Header from './header';
 import Footer from './footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
-    <App />
-    <Footer />
+    <div className='flex flex-col min-h-screen'>
+      <Header />
+      <IndexView />
+      <Footer />
+    </div>
   </React.StrictMode>
 );
