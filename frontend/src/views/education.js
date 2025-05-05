103d24daa3453840d2c91fee68ebdb655a5d59ff
Nicole <nholt3@atu.edu>
2025-05-04 20:37:35 -0500

    completed education page

--- frontend/src/views/education.js
+++ frontend/src/views/education.js
@@ -1,18 +1,9 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import EducationView from './educationView';
 import Header from '../components/header';
 import Footer from '../components/footer';
 
 function Education(){
-  useEffect(() => {
-      fetch('http://localhost:5000/education')
-      .then(res => res.text())
-      .then(data => {
-        console.log('GET /education response:', data);
-      })
-      .catch(err => console.error('GET /education failed:', err));
-    }, []);
-
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
--- frontend/src/views/educationView.js
+++ frontend/src/views/educationView.js
@@ -1,9 +1,25 @@
+import React, {useEffect, useState} from 'react';
+
 function EducationView() {
+    const [education, setEducation] = useState([]);
+    
+    useEffect(() => {
+        fetch('http://localhost:5000/education')
+        .then(res => res.json())
+        .then(data => setEducation(data))
+        .catch(err => console.error('Failed to fetch education:', err));
+    }, []);
+
     return (
-      <main className="text-darkestcolor dark:text-lightestcolor">
-        <div className="bg-lightcolor dark:bg-darkcolor mx-auto my-10 rounded-xl border-6 border-accentcolor p-8 w-[90%] flex flex-col items-center gap-6">
-            <p>Testing</p>
-        </div>
+        <main className="text-darkestcolor dark:text-lightestcolor">
+        {education.map((education, index) => (
+            <div className="bg-lightcolor dark:bg-darkcolor mx-auto my-10 rounded-xl border-6 border-accentcolor p-8 w-[90%] flex flex-col items-center gap-4">
+                <h2 className='font-bold text-4xl text-center'>{education.degree}</h2>
+                <p className='text-2xl'>Institution: {education.institution}</p>
+                <p className='text-lg'>Year: {education.gradYear}</p>
+                <p>Description: {education.description}</p>
+            </div>
+        ))}
       </main>
     );
 }
