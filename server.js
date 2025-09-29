b9e7c4cf87ee45cd9905583a35cc52d0194f5d42
Nicole <nholt3@atu.edu>
2025-09-29 01:40:12 -0500

    Moved from local MongoDB to MongoDB Atlas

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
+require("dotenv").config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
 const PORT = 5000;
+const uri = process.env.MONGO_URI;
 
 app.use(cors());
 app.use(express.json());
 
 //Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/resume', {
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
