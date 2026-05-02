ae86dca76dbc97e195968b06f24fe731a2e0e573
Nicole Holt <nicoleholt90888@gmail.com>
2026-05-02 01:02:52 -0500

    Updated skills.jsx UI to have the boxes in multiple columns on larger views

--- portfolio/src/views/skills.jsx
+++ portfolio/src/views/skills.jsx
@@ -22,9 +22,9 @@ function SkillsView() {
     }, []);
 
     return (
-        <main className="text-darkestcolor dark:text-lightestcolor">
+      <main className="flex flex-col md:flex-row md:flex-wrap justify-center gap-6">
         {skills.map((skills, index) => (
-            <div className="bg-lightcolor dark:bg-darkcolor mx-auto my-10 rounded-xl border-6 border-accentcolor p-8 w-[90%] flex flex-col items-center gap-6">
+            <div className="bg-lightcolor dark:bg-darkcolor mx-auto my-10 rounded-xl border-6 border-accentcolor p-8 w-[70%] md:w-[30%] flex flex-col items-center gap-6">
                 <h2 className='font-bold text-4xl text-center'>Skill: {skills.skill}</h2>
                 <p className='text-xl'>Proficiency: {skills.proficiency}</p>
                 {skills.icon && <img src={skills.icon} alt={skills.skill} className="bg-lightestcolor rounded-md" />}
