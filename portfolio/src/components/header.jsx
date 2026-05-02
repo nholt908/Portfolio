850ffc7fd6f4b8ee1ac2d32d2bd189c0987acd93
Nicole Holt <nicoleholt90888@gmail.com>
2026-05-01 22:41:43 -0500

    change <a> to <Link> in header.jsx

--- portfolio/src/components/header.jsx
+++ portfolio/src/components/header.jsx
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import {Menu, X} from "lucide-react";
+import { Link } from "react-router-dom";
 
 function Header(){
     const [menuOpen, setMenuOpen] = useState(false);
@@ -12,20 +13,20 @@ function Header(){
                 <button className="md:hidden" onClick={() => setMenuOpen(!menuOpen)} aria-label="Toggle menu">{menuOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}</button>
             
                 <ul className="hidden md:flex flex-row gap-6 lg:gap-12 lg:text-xl">
-                    <li><a className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold" href="/">Home</a></li>
-                    <li><a className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold" href="/education">Education</a></li>
-                    <li><a className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold" href="/skills">Skills</a></li>
-                    <li><a className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold" href="/projects">Projects</a></li>
-                    <li><a className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold" href="/contact">Contact Me</a></li>
+                    <li><Link to="/" className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold">Home</Link></li>
+                    <li><Link to="/education" className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold">Education</Link></li>
+                    <li><Link to="/skills" className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold">Skills</Link></li>
+                    <li><Link to="/projects" className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold">Projects</Link></li>
+                    <li><Link to="/contact" className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold">Contact Me</Link></li>
                 </ul>
             </div>
                 {menuOpen && (
                     <ul className="flex flex-col items-center gap-6 py-4 md:hidden">
-                        <li><a className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold" href="/">Home</a></li>
-                        <li><a className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold" href="/education">Education</a></li>
-                        <li><a className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold" href="/skills">Skills</a></li>
-                        <li><a className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold" href="/projects">Projects</a></li>
-                        <li><a className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold" href="/contact">Contact Me</a></li>
+                        <li><Link to="/" className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold">Home</Link></li>
+                        <li><Link to="/education" className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold">Education</Link></li>
+                        <li><Link to="/skills" className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold">Skills</Link></li>
+                        <li><Link to="/projects" className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold">Projects</Link></li>
+                        <li><Link to="/contact" className="px-4 py-2 rounded-2xl border-3 border-accentcolor hover:bg-lightestcolor hover:dark:bg-darkestcolor hover:font-bold">Contact Me</Link></li>
                     </ul>
                 )}
             
