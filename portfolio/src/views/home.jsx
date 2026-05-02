24c34314d2471e2104eab74c50ae9978cca94475
Nicole Holt <nicoleholt90888@gmail.com>
2026-05-02 00:02:28 -0500

    Update professional photo

--- portfolio/src/views/home.jsx
+++ portfolio/src/views/home.jsx
@@ -24,7 +24,7 @@ function HomeView() {
   return (
     <main>
       <div className="bg-lightcolor dark:bg-darkcolor mx-auto my-10 rounded-xl border-6 border-accentcolor p-8 w-[90%] flex flex-col md:flex-row items-center gap-6">
-        <img src="/Nicole.jpg" alt="Nicole"className="border-3 rounded-3xl border-accentcolor w-full md:w-1/2" />
+        <img src="/Nicole.png" alt="Nicole"className="border-3 rounded-3xl border-accentcolor w-full md:w-1/2" />
         <p className="md:max-w-1/2">My name is Nicole, and I am a current student at Arkansas Tech University pursuing a degree in Computer Science. With a foundation in Website Development, I am passionate about learning and growing within this field. Throughout my academic journey, I have developed skills in various programming languages which can be found on the skills page. On the projects page, you will find a few projects which I have completed. I am excited to apply my knowledge and creativity as I am given challenges and opportunities to develope professionally.</p>
       </div>
       <div className="bg-lightcolor dark:bg-darkcolor mx-auto my-10 rounded-xl border-6 border-accentcolor p-8 w-[90%] flex flex-col items-center gap-6">
